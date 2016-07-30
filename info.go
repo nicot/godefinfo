3c4ef1d8dc54cc9f7d2314c05dcbe5bfbbec352e
Nico Tonozzi <nico@nicot.us>
2016-07-29 17:16:25 -0700

    info stuff

--- info.go
+++ info.go
@@ -6,16 +6,19 @@ import (
 	"os"
 	"strings"
 
+	"path/filepath"
+
 	"github.com/golang/gddo/gosrc"
 )
 
 type defInfo struct {
 	Package  string
 	Path     string
-	Field    string
 	Repo     string
 	Unit     string
 	UnitType string
+	Kind     string
+	File     string
 }
 
 func outputData(data ...interface{}) {
@@ -25,8 +28,10 @@ func outputData(data ...interface{}) {
 		return
 	}
 	datas := strings.Split(strings.Trim(output, "\n"), " ")
+	file := filepath.Base(*filename)
 	info := defInfo{
 		UnitType: "GoPackage",
+		File:     file,
 	}
 	if len(datas) > 0 {
 		info = setRepo(info, datas[0])
@@ -35,15 +40,21 @@ func outputData(data ...interface{}) {
 		info.Path = datas[1]
 	}
 	if len(datas) > 2 {
-		info.Field = datas[2]
+		field := datas[2]
+		info.Path = info.Path + "/" + field
+	}
+	if info.Path == "" {
+		info.Kind = "package"
 	}
 	json.NewEncoder(os.Stdout).Encode(info)
+	// TODO main package files
+	// TODO package tree urls
 }
 
 func setRepo(info defInfo, fullPath string) defInfo {
 	// The funny thing is none of these are packages
 	if gosrc.IsGoRepoPath(fullPath) {
-		info.Repo = "github.com/golang/go/"
+		info.Repo = "github.com/golang/go"
 	} else {
 		paths := strings.Split(fullPath, "/")
 		paths = paths[0:3]
@@ -54,3 +65,6 @@ func setRepo(info defInfo, fullPath string) defInfo {
 	info.Unit = fullPath
 	return info
 }
+
+var x int
+var y string
